9b4d508c9dccd89077303992492b164100a05215
quorum <micjph@protonmail.com>
2021-01-01 16:16:57 +0100

    Tab kind updated

--- spinal/src/main/scala/rc800/Vectors.scala
+++ spinal/src/main/scala/rc800/Vectors.scala
@@ -4,10 +4,10 @@ import spinal.core._
 import spinal.lib._
 
 object Vectors {
-    val Reset = 0x0000
-    val NonMaskableInterrupt = 0x0008
-    val ExternalInterrupt = 0x0010
-    val IllegalInstruction = 0x0018
-    val StackOverflow = 0x0020
-    val IllegalInterrupt = 0x0028
+	val Reset = 0x0000
+	val NonMaskableInterrupt = 0x0008
+	val ExternalInterrupt = 0x0010
+	val IllegalInstruction = 0x0018
+	val StackOverflow = 0x0020
+	val IllegalInterrupt = 0x0028
 }
